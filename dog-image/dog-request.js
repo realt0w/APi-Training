bc4abba2508883cf388964c6e2408765084e11e4
Tom ツ <emodzz@yahoo.com>
2023-02-04 05:49:29 +0100

    Dog Image

---
+++ dog-image/dog-request.js
@@ -0,0 +1,27 @@
+function makeApiRequest() {
+    // API endpoint
+    var url = 'https://dog.ceo/api/breeds/image/random';
+
+    // Make the API request using the Fetch API
+    fetch(url)
+      .then(function(response) {
+        // Check if the API request was successful
+        if (response.ok) {
+          // Extract the value from the API response
+          return response.json().then(function(data) {
+            var value = data.message;
+
+            // Display the value
+            document.getElementById("output").src = value;
+          });
+        } else {
+          
+          // Display an error message if the API request was unsuccessful
+          document.getElementById("output").innerHTML = "Error: API request failed with status code " + response.status;
+        }
+      })
+      .catch(function(error) {
+        // Display an error message if there was a problem with the API request
+        document.getElementById("output").innerHTML = "Error: " + error;
+      });
+  }
